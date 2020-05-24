acb0dba091fe21708ef85964a293cd946a5eb625
Adam Osburn <wizardofosburn@gmail.com>
2020-05-24 17:10:45 -0400

    added label, input for name element, added label, select, option for size element

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -3,6 +3,22 @@ import React from 'react';
 export default function Form(){
 
     return(
-        
+        <form onSubmit={submit}>
+            <label htmlFor='name'>
+                Name:
+
+                <input id='name' name='name' placeholder='Please provide a name' value={}/>
+            </label>
+
+            <label htmlFor='size'>
+                Choose a pizza size: 
+                <select id='size' name={size} selected='medium' value={} >
+                    {formState.size.map(size => (
+                        <option key='size.id' name={size} value={size}>{size}
+                        </option>
+                    ))}
+                </select>
+            </label>
+        </form>
     )
 }
