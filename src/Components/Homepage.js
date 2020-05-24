45d8dbb122bd2b6c805bc610a24273f2684d9744
Adam Osburn <wizardofosburn@gmail.com>
2020-05-24 17:11:24 -0400

    imported link, added link to /pizza path on Homepage

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 export default function Homepage() {
 
     return (
-        
+        <Link to='/pizza'>Build Your Own Pizza</Link>
     )
 }
