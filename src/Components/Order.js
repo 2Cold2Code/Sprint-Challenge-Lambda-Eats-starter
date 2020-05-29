a85fa37336415cb32940836faff2db38dab6a0f4
Adam Osburn <wizardofosburn@gmail.com>
2020-05-28 22:45:22 -0400

    wrapped card in div

--- src/Components/Order.js
+++ src/Components/Order.js
@@ -11,22 +11,24 @@ import React from 'react'
 export default function Order(props){
     const [order] = props;
     return (
-        <h1 className='name-label'>Name: </h1>
-        <p className='name'>{order.name}</p>
-        <h3>Size: </h3>
-        <p>{order.size}</p>
-        <h3>Sauce: </h3>
-        <p>{order.sauce}</p>
-        <h3>Toppings: </h3>
-        <ol className='toppings-container'>
+        <div className='order-card'>
+          <h1 className='name-label'>Name: </h1>
+          <p className='name'>{order.name}</p>
+          <h3>Size: </h3>
+          <p>{order.size}</p>
+          <h3>Sauce: </h3>
+          <p>{order.sauce}</p>
+          <h3>Toppings: </h3>
+          <ol className='toppings-container'>
             {order.toppings.map(topping =>(
                 <li>
                     <p>{topping}</p>
                 </li>
                 )
-            )
-        </ol>
-        <h3>Special Instructions: </h3>
-        <p>{order.special}</p>
+            )}
+          </ol>
+          <h3>Special Instructions: </h3>
+          <p>{order.special}</p>
+        </div>
     )
 }
